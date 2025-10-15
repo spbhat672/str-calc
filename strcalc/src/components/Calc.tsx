80a57c9a6c55e86ab35cc8666f2fc59e559c6a0c
Santosh Bhat <santoshbhat672@gmail.com>
2025-10-15 20:00:48 +0530

    Added Interface

---
+++ strcalc/src/components/Calc.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { addNumbers } from '../utils/calculator';
+
+const Calc = () => {
+    const [val, setVal] = useState<string>("");
+    const [result, setResult] = useState<number>();
+
+    const handleClick = () => {
+        const res = addNumbers(val);
+        setResult(res);
+    }
+
+    return (
+        <>
+            <div>
+                <div style={{ height: '40vmin' }}>
+                    <div className='d-flex align-items-center justify-content-between'>
+                        <input onChange={(e) => setVal(e.target.value)} style={{ marginRight: '10px' }} />
+                        <button style={{ marginLeft: '10px' }} onClick={handleClick}>Submit</button>
+                    </div>
+                </div>
+                <div className='mt-3'>
+                    <label className='d-flex align-items-center justify-content-center'>{result}</label>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Calc;
---
+++ strcalc/src/utils/calculator.ts
@@ -0,0 +1,8 @@
+function isCharADigit(char: string): boolean {
+  return /^[0-9]$/.test(char);
+}
+
+export function addNumbers(input: string) {
+  if (input.length === 0) return 0;
+}
+
