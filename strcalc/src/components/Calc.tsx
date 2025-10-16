35b8948b3081472cfec447ce3388137226405a37
Santosh Bhat <santoshbhat672@gmail.com>
2025-10-16 16:51:59 +0530

    final touch

--- strcalc/src/components/Calc.tsx
+++ strcalc/src/components/Calc.tsx
@@ -13,9 +13,10 @@ const Calc = () => {
     return (
         <>
             <div style={{ height: '40vmin' }}>
+                <div className='d-flex justify-content-center mb-4'>String Calculator</div>
                 <div className='d-flex align-items-center justify-content-between'>
                     <input onChange={(e) => setVal(e.target.value)} style={{ marginRight: '10px' }} />
-                    <button style={{ marginLeft: '10px' }} onClick={handleClick}>Submit</button>
+                    <button style={{ marginLeft: '10px' }} onClick={handleClick}>Add</button>
                 </div>
                 <div className='mt-3'>
                     <label className='d-flex align-items-center justify-content-center'>{result}</label>
